f98bce2331488f837f0ddd99570da17fcde7d7b3
AlexCen29 <conalepp@gmail.com>
2025-06-22 21:19:15 -0600

    Alex - deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), ghPages(),
     vueDevTools(),
   ],
+  base: '/blog-noticas2/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
